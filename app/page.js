094a4d4650c20e5a338b999d7955fee50120487f
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-11-07 19:28:37 +0500

    fix mini

--- app/page.js
+++ app/page.js
@@ -8,17 +8,48 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center bg-[#0F0F0F] text-white overflow-hidden">
-      <img src="/greenbg.svg" alt="bg" className="w-full z-0 absolute top-0 object-cover"/>
+      <img
+        src="/greenbg.svg"
+        alt="bg"
+        className="w-full z-0 absolute top-0 object-cover"
+      />
       <Navbar />
       <Hero />
       <p className="sm:text-center max-w-[1390px] text-[14px] text-justify sm:text-2xl pt-10 lg:pt-4 pb-10 w-[96%] lg:w-full">
-      Enter the realm of Ai1, where the future of automated algorithmic trading is being reshaped by artificial intelligence. At its heart lies Ai1, a comprehensive trading system, forged from a fusion of advanced technologies and innovative market methodologies.      </p>
+        Enter the realm of Ai1, where the future of automated algorithmic
+        trading is being reshaped by artificial intelligence. At its heart lies
+        Ai1, a comprehensive trading system, forged from a fusion of advanced
+        technologies and innovative market methodologies.{" "}
+      </p>
       <div className="relative w-full flex items-center justify-center">
-      <img src="/codebg.svg" alt="bg" className="w-full absolute z-0 top-0 left-0 object-cover"/>
-      <Example />
+        <img
+          src="/codebg.svg"
+          alt="bg"
+          className="w-full hidden lg:flex absolute z-0 top-0 left-0 object-cover"
+        />
+        <img
+          src="/codeMobilebg.svg"
+          alt="bg"
+          className="w-full blur-sm h-full flex lg:hidden absolute z-0 top-0 left-0 object-cover"
+        />
+        <Example />
       </div>
       <Info />
-      <About />
+      <div className="relative w-full flex flex-col items-center justify-center">
+        
+        <About />
+        
+        <img
+          src="/aboutbg.svg"
+          alt="bg"
+          className="w-full blur-sm hidden sm:flex absolute z-0 top-0 left-0 object-cover"
+        />
+        <img
+          src="/codeMobilebg.svg"
+          alt="bg"
+          className="w-full blur-md h-[110%] flex sm:hidden absolute z-0 top-0 left-0 object-cover"
+        />
+      </div>
       <Join />
     </main>
   );
--- components/About.jsx
+++ components/About.jsx
@@ -1,11 +1,13 @@
 'use client'
 import React from 'react'
 import {motion} from 'framer-motion'
+import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
+
 
 const About = () => {
   return (
     <>
-    <div className='hidden lg:grid grid-cols-1 lg:grid-cols-3 max-w-[1290px] mt-10 w-[96%] lg:w-full'>
+    <div className='hidden lg:grid grid-cols-1 lg:grid-cols-3 max-w-[1290px] z-10 mt-10 w-[96%] lg:w-full'>
         <motion.div initial="hidden"
       whileInView="visible"
       viewport={{ once: true }}
@@ -13,9 +15,9 @@ const About = () => {
       variants={{
         visible: { opacity: 1, translateX: '12px' },
         hidden: { opacity: 0, translateX: '100px' }
-      }} className='bg-gradient-to-r col-span-1 flex flex-col gap-4 translate-x-3 from-[#31353D] to-[#292929] rounded-xl border border-[#38453F] p-5'>
+      }} className='bg-[#3b3b3b]  bg-opacity-60 col-span-1 flex flex-col gap-4 translate-x-3 backdrop-blur-lg rounded-xl border border-[#3b3b3b] p-5'>
             <h3 className='font-bold text-[24px] upperase bg-clip-text text-transparent bg-gradient-to-r from-white to-[#AFAFAF]'>TOTAL SUPPLLY</h3>
-            <p className='text-[#ABABAB] text-[20px]'>100.000.000 Ai1 tokens</p>
+            <p className='text-[#ABABAB]'>100.000.000 Ai1 tokens</p>
         </motion.div>
         <motion.div  initial="hidden"
       whileInView="visible"
@@ -24,11 +26,11 @@ const About = () => {
       variants={{
         visible: { opacity: 1, translateX: '-12px' },
         hidden: { opacity: 0, translateX: '100px' }
-      }}  className='bg-gradient-to-r col-span-1 flex flex-col -translate-x-3 from-[#31353D] to-[#292929] rounded-xl border border-[#38453F] p-5'>
+      }}  className='bg-[#3b3b3b]  bg-opacity-60 col-span-1 flex flex-col -translate-x-3 backdrop-blur-lg  rounded-xl border border-[#3b3b3b] p-5'>
             <h3 className='font-bold text-[24px] upperase mb-4 bg-clip-text text-transparent  bg-gradient-to-r from-white to-[#AFAFAF]'>TAXES</h3>
-            <p className='text-[#ABABAB] text-[20px]'>Transfer, Buy and Sell: 6%</p>
-            <p className='text-[#ABABAB] text-[20px]'>3% development</p>
-            <p className='text-[#ABABAB] text-[20px]'>3% holder rewards (lottery <br/>mechanism)</p>
+            <p className='text-[#ABABAB]'>Transfer, Buy and Sell: 6%</p>
+            <p className='text-[#ABABAB]'>3% development</p>
+            <p className='text-[#ABABAB]'>3% holder rewards (lottery <br/>mechanism)</p>
         </motion.div>
         <motion.div  initial="hidden"
       whileInView="visible"
@@ -37,10 +39,22 @@ const About = () => {
       variants={{
         visible: { opacity: 1, translateX: '-24px' },
         hidden: { opacity: 0, translateX: '100px' }
-      }} className='bg-gradient-to-r col-span-1 flex flex-col -translate-x-6 gap-4 from-[#31353D] to-[#292929] rounded-xl border border-[#38453F] p-5'>
+      }} className='bg-[#3b3b3b]  bg-opacity-60 col-span-1 flex flex-col -translate-x-6 gap-4 backdrop-blur-lg  rounded-xl border border-[#3b3b3b] p-5'>
             <h3 className='font-bold text-[24px] upperase bg-clip-text text-transparent  bg-gradient-to-r from-white to-[#AFAFAF]'>LIQUIDITY</h3>
-            <p className='text-[#ABABAB] text-[20px]'>Locked on Uncrypt</p>
+            <p className='text-[#ABABAB]'>Locked on Uncrypt</p>
         </motion.div>
+        <motion.a initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay:0.8 }}
+      variants={{
+        visible: { opacity: 1 },
+        hidden: { opacity: 0}
+      }} 
+          className="rounded-full ml-3 sm:self-start z-10 mt-5 px-4 py-2 w-fit bg-[#676809] duration-100 ease-linear transition-all hover:shadow-[0px_0px_30px_2px_#676809] flex items-center gap-3 shadow-[0px_0px_0px_0px_#676809]"
+          href={"#"}>
+          <p>Documentation</p> <ArrowUpRightIcon width={18} h={18} />
+        </motion.a>
     </div>
     <div className='grid lg:hidden grid-cols-1 gap-4 lg:grid-cols-3 max-w-[1290px] mt-10 w-[96%] lg:w-full'>
         <motion.div initial="hidden"
@@ -50,9 +64,9 @@ const About = () => {
       variants={{
         visible: { opacity: 1, translateY: 0 },
         hidden: { opacity: 0, translateY: '40px' }
-      }} className='bg-gradient-to-r col-span-1 flex flex-col gap-4 from-[#31353D] to-[#292929] rounded-xl border border-[#38453F] p-[10px] sm:p-5'>
+      }} className='bg-[#3b3b3b]  bg-opacity-60 col-span-1 flex flex-col gap-4   backdrop-blur-lg  rounded-xl border border-[#3b3b3b] p-[10px] sm:p-5'>
             <h3 className='font-bold text-[24px] upperase bg-clip-text text-transparent bg-gradient-to-r from-white to-[#AFAFAF]'>TOTAL SUPPLLY</h3>
-            <p className='text-[#ABABAB] text-[20px]'>100.000.000 Ai1 tokens</p>
+            <p className='text-[#ABABAB]'>100.000.000 Ai1 tokens</p>
         </motion.div>
         <motion.div  initial="hidden"
       whileInView="visible"
@@ -61,11 +75,11 @@ const About = () => {
       variants={{
         visible: { opacity: 1, translateY: 0 },
         hidden: { opacity: 0, translateY: '40px' }
-      }}  className='bg-gradient-to-r col-span-1 flex flex-col  from-[#31353D] to-[#292929] rounded-xl border border-[#38453F] p-[10px] sm:p-5'>
+      }}  className='bg-[#3b3b3b]  bg-opacity-60 col-span-1 flex flex-col    backdrop-blur-lg  rounded-xl border border-[#3b3b3b] p-[10px] sm:p-5'>
             <h3 className='font-bold text-[24px] upperase mb-4 bg-clip-text text-transparent  bg-gradient-to-r from-white to-[#AFAFAF]'>TAXES</h3>
-            <p className='text-[#ABABAB] text-[20px]'>Transfer, Buy and Sell: 6%</p>
-            <p className='text-[#ABABAB] text-[20px]'>3% development</p>
-            <p className='text-[#ABABAB] text-[20px]'>3% holder rewards (lottery <br/>mechanism)</p>
+            <p className='text-[#ABABAB]'>Transfer, Buy and Sell: 6%</p>
+            <p className='text-[#ABABAB]'>3% development</p>
+            <p className='text-[#ABABAB]'>3% holder rewards (lottery <br/>mechanism)</p>
         </motion.div>
         <motion.div  initial="hidden"
       whileInView="visible"
@@ -74,10 +88,22 @@ const About = () => {
       variants={{
         visible: { opacity: 1, translateY: 0 },
         hidden: { opacity: 0, translateY: '40px' }
-      }} className='bg-gradient-to-r col-span-1 flex flex-col gap-4 from-[#31353D] to-[#292929] rounded-xl border border-[#38453F] p-[10px] sm:p-5'>
+      }} className='bg-[#3b3b3b]  bg-opacity-60 col-span-1 flex flex-col gap-4  backdrop-blur-lg  rounded-xl border border-[#3b3b3b] p-[10px] sm:p-5'>
             <h3 className='font-bold text-[24px] upperase bg-clip-text text-transparent  bg-gradient-to-r from-white to-[#AFAFAF]'>LIQUIDITY</h3>
-            <p className='text-[#ABABAB] text-[20px]'>Locked on Uncrypt</p>
+            <p className='text-[#ABABAB]'>Locked on Uncrypt</p>
         </motion.div>
+        <motion.a initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.5, delay:0.8 }}
+      variants={{
+        visible: { opacity: 1},
+        hidden: { opacity: 0 }
+      }} 
+          className="rounded-full justify-self-center z-10 mt-5 px-4 py-2 w-fit bg-[#676809] duration-100 ease-linear transition-all hover:shadow-[0px_0px_30px_2px_#676809] flex items-center gap-3 shadow-[0px_0px_0px_0px_#676809]"
+          href={"#"}>
+          <p>Documentation</p> <ArrowUpRightIcon width={18} h={18} />
+        </motion.a>
     </div>
     </>
     
